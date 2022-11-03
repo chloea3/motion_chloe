ada4c24ee976a1dcdb648e21bee0e97ffbc9ec19
chloea3 <102743778+chloea3@users.noreply.github.com>
2022-11-03 12:19:58 -0400

    added gsap

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,3 +1,18 @@
 import { gsap } from "gsap";
 
 
+import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
+import { GSDevTools } from "gsap/GSDevTools";
+
+
+gsap.registerPlugin(DrawSVGPlugin, GSDevTools);
+
+
+function simpleMotion(){
+    var tl = gsap.timeline();
+    tl.to("#Frame",{duration:5, fill:"#1f1fff", x: 200});
+    return tl;
+}
+
+var mainTL = gsap.timeline();
+mainTL.add(simpleMotion())
