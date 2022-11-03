247ec4cb6a3930a1cb738df4e29a90391fb451c4
chloea3 <102743778+chloea3@users.noreply.github.com>
2022-11-03 03:13:32 -0400

    star added

---
+++ dev/js/scripts.js
@@ -0,0 +1,8 @@
+import { gsap } from "gsap";
+import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
+import { GSDevTools } from "gsap/GSDevTools";
+
+gsap.registerPlugin(DrawSVGPlugin, GSDevTools);
+
+
+gsap.to("#Star",{duration:3, x:900, scale:7});
