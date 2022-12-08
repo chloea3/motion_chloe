3dec3ab479a22100add38d85b71490827eaac3bb
chloea3 <102743778+chloea3@users.noreply.github.com>
2022-12-08 04:45:06 -0500

    animation adds

---
+++ yoodel-min.js
@@ -0,0 +1,1 @@
+const tl=gsap.timeline();tl.staggerFrom([".y",".o1",".o2",".d",".e",".l"],1.5,{scale:0,transformOrigin:"bottom",ease:Elastic.easeOut},.3),tl.delay(0),tl.staggerFrom([".topL",".topR",".botL",".botR",".midL",".midR"],1.5,{scale:0,transformOrigin:"top center",ease:"Elastic.easeOut"});
